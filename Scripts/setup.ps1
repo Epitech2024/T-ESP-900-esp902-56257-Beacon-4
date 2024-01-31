271196bc6ef050d9bdd2d20a3b3deb03132c5af5
IronJulo <jules.brss@gmail.com>
2024-01-31 01:04:19 +0100

    feat: edit setup script to output utf8
    Thanks Windows

--- Scripts/setup.ps1
+++ Scripts/setup.ps1
@@ -10,13 +10,13 @@ $protocol_version = Get-Content '.\protocol\protocol.version' -Raw
 # mkdir .\DeviceFirmware\Arduino_Firmware_src\src\embedded_proto
 protoc --plugin=protoc-gen-eams=.\EmbeddedProto\protoc-gen-eams.bat -I .\Protocol\ --eams_out=.\DeviceFirmware\Arduino_Firmware_src\src\protocol protocol.proto
 cp .\EmbeddedProto\src\* .\DeviceFirmware\Arduino_Firmware_src\src\embedded_proto
-"#ifndef PROTOCOL_VERSION_H`n`#define PROTOCOL_VERSION_H`n`const char *protocolVersion = `"$protocol_version`";`n`#endif /* PROTOCOL_VERSION_H */" | Out-File '.\DeviceFirmware\Arduino_Firmware_src\src\protocol\protocolversion.h'
+"#ifndef PROTOCOL_VERSION_H`n`#define PROTOCOL_VERSION_H`n`const char *protocolVersion = `"$protocol_version`";`n`#endif /* PROTOCOL_VERSION_H */" | Out-File '.\DeviceFirmware\Arduino_Firmware_src\src\protocol\protocolversion.h' -Encoding UTF8
 
 # Setup GatewayFirmware
 # mkdir .\GatewayFirmware\Arduino_Firmware_src\src\protocol
 # mkdir .\GatewayFirmware\Arduino_Firmware_src\src\embedded_proto
 protoc --plugin=protoc-gen-eams=.\EmbeddedProto\protoc-gen-eams.bat -I .\Protocol\ --eams_out=.\GatewayFirmware\Arduino_Firmware_src\src\protocol protocol.proto
 cp .\EmbeddedProto\src\* .\GatewayFirmware\Arduino_Firmware_src\src\embedded_proto
-"#ifndef PROTOCOL_VERSION_H`n`#define PROTOCOL_VERSION_H`n`const char *protocolVersion = `"$protocol_version`";`n`#endif /* PROTOCOL_VERSION_H */" | Out-File '.\GatewayFirmware\Arduino_Firmware_src\src\protocol\protocolversion.h'
+"#ifndef PROTOCOL_VERSION_H`n`#define PROTOCOL_VERSION_H`n`const char *protocolVersion = `"$protocol_version`";`n`#endif /* PROTOCOL_VERSION_H */" | Out-File '.\GatewayFirmware\Arduino_Firmware_src\src\protocol\protocolversion.h' -Encoding UTF8
 
 
